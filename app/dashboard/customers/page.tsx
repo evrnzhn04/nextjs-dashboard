e87b15e583f4032825e044a0d8215e5fb86d312d
evrnzhn04 <110024215+evrnzhn04@users.noreply.github.com>
2025-09-20 11:10:42 +0300

    V1.0

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,149 @@
 import { Metadata } from 'next';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import Image from 'next/image';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page(){
-    return <p>Customer Page</p>
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: Promise<{
+    query?: string;
+  }>;
+}) {
+  const resolvedSearchParams = await searchParams;
+  const query = resolvedSearchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Customers</h1>
+      </div>
+      
+      {/* Search Bar */}
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <form className="relative flex flex-1 flex-shrink-0">
+          <input
+            className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+            placeholder="Search customers..."
+            name="query"
+            defaultValue={query}
+          />
+          <svg
+            className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900"
+            width="18"
+            height="18"
+            viewBox="0 0 24 24"
+            fill="none"
+          >
+            <path
+              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </form>
+      </div>
+
+      {/* Customer Table */}
+      <div className="mt-6 flow-root">
+        <div className="inline-block min-w-full align-middle">
+          <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
+            <div className="md:hidden">
+              {customers?.map((customer) => (
+                <div
+                  key={customer.id}
+                  className="mb-2 w-full rounded-md bg-white p-4"
+                >
+                  <div className="flex items-center justify-between border-b pb-4">
+                    <div>
+                      <div className="mb-2 flex items-center">
+                        <Image
+                          src={customer.image_url}
+                          className="mr-2 rounded-full"
+                          width={28}
+                          height={28}
+                          alt={`${customer.name}'s profile picture`}
+                        />
+                        <p>{customer.name}</p>
+                      </div>
+                      <p className="text-sm text-gray-500">{customer.email}</p>
+                    </div>
+                  </div>
+                  <div className="flex w-full items-center justify-between pt-4">
+                    <div>
+                      <p className="text-xl font-medium">
+                        {customer.total_invoices} invoices
+                      </p>
+                      <p>Paid: {customer.total_paid}</p>
+                      <p>Pending: {customer.total_pending}</p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <table className="hidden min-w-full text-gray-900 md:table">
+              <thead className="rounded-lg text-left text-sm font-normal">
+                <tr>
+                  <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
+                    Customer
+                  </th>
+                  <th scope="col" className="px-3 py-5 font-medium">
+                    Email
+                  </th>
+                  <th scope="col" className="px-3 py-5 font-medium">
+                    Total Invoices
+                  </th>
+                  <th scope="col" className="px-3 py-5 font-medium">
+                    Total Pending
+                  </th>
+                  <th scope="col" className="px-4 py-5 font-medium">
+                    Total Paid
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white">
+                {customers?.map((customer) => (
+                  <tr
+                    key={customer.id}
+                    className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
+                  >
+                    <td className="whitespace-nowrap py-3 pl-6 pr-3">
+                      <div className="flex items-center gap-3">
+                        <Image
+                          src={customer.image_url}
+                          className="rounded-full"
+                          width={28}
+                          height={28}
+                          alt={`${customer.name}'s profile picture`}
+                        />
+                        <p>{customer.name}</p>
+                      </div>
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-3">
+                      {customer.email}
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-3">
+                      {customer.total_invoices}
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-3">
+                      {customer.total_pending}
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-3">
+                      {customer.total_paid}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
